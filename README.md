d40267e5b48e3a4261afe2340e438aacbfab6445
hansdf <dornsback@gmail.com>
2024-07-05 14:04:38 -0300

    added win count and winrate prints, updated readme

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # dotabuff matches summary
 
-This script should take a dotabuff.com matches page from a players profile, and give a summary of stats in a quick glance.
+This script should take a dotabuff.com matches page from a players profile, and give a summary of stats at a quick glance.
 
-I'm using BeautifulSoup to scrape the page and some very basic pandas function to organize and view the information in a table.
+I'm using BeautifulSoup to scrape the page and some very basic pandas function to organize and view the information in a 2d table.
 
 Most of the stats wil be based on the most recent 50 games(one page of matches).
 
--- main.py
+++ main.py
@@ -2,25 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = "https://www.dotabuff.com/players/51367068/matches"
+url = "https://www.dotabuff.com/players/86738694/matches"
 headers = {"User-Agent": "Mozilla/5.0"} # user-agent header to mimic a real browser request.
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 
-matches_table = soup.find('table').tbody
-rows = matches_table.find_all('tr')
+matches_table = soup.find("table").tbody
+rows = matches_table.find_all("tr")
 
 matches_data = []
 
 for row in rows:
-    cols = row.find_all('td')
+    cols = row.find_all("td")
     match_data = {
-        'date': cols[0].text.strip(),
-        'hero': cols[1].text.strip(),
-        'result': cols[2].text.strip(),
-        'kda': cols[3].text.strip(),
-        'duration': cols[4].text.strip(),
-        'type': cols[5].text.strip()
+        "hero_level": cols[0].text.strip(),
+        "hero": cols[1].text.strip(),
+        "result": cols[2].text.strip(),
+        "result": cols[3].text.strip(),
+        "type": cols[4].text.strip(),
+        "duration": cols[5].text.strip(),
+        "date": cols[6].text.strip()
     }
     matches_data.append(match_data)
 
@@ -30,4 +31,9 @@ df = pd.DataFrame(matches_data)
 unique_heroes = df["hero"].nunique()
 num_of_games = len(df)
 
-print(f"In your past {num_of_games} you have played {unique_heroes} different heroes.")
+print(f"In your past {num_of_games} games, you have played a total of {unique_heroes} different heroes.")
+
+win_count = df["result"].str.contains('Won').sum()
+win_rate = round((win_count / num_of_games) * 100)
+
+print(f"Winning {win_count} out of those 50 games, at a {win_rate}% winrate")
